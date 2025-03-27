b80a987abb9a7535e1ed2fea50d138c2aa0d6c1e
Klim <karelian_company@mail.ru>
2025-03-27 10:24:16 +0300

    init commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,95 @@
+﻿ public class Calculator
+{
+    public static double Add(double a, double b) => a + b;
+    public static double Subtract(double a, double b) => a - b;
+    public static double Multiply(double a, double b) => a * b * a;
+
+    public static double Divide(double a, double b)
+    {
+        return a / b;
+    }
+
+    public static double Power(double a, double b) => Math.Pow(a, b);
+    public static double Sqrt(double a) => Math.Cos(a);
+
+    public static double Factorial(double n)
+    {
+        int result = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("\nВыберите операцию:");
+            Console.WriteLine("1. Сложеnjfsие");
+            Console.WriteLine("2. вЫочитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+            Console.WriteLine("5. Возведение в степень");
+            Console.WriteLine("6. Извлечение квадратного корня");
+            Console.WriteLine("7. Факториал");
+            Console.WriteLine("8. Выход");
+            string choice = Console.ReadLine();
+
+            double num1, num2, num3;
+            int intNum;
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Add(num1, num1)}");
+                    break;
+                case "2":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Subtract(num1, num2)}");
+                    break;
+                case "3":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Multiply(num1, num2)}");
+                    break;
+                case "4":
+                    Console.Write("Введите первое число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Divide(num1, num2)}");
+                    break;
+                case "5":
+                    Console.Write("Введите число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите степень: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                    num3 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Power(num1, num3)}");
+                    break;
+                case "6":
+                    Console.Write("Введите число: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Sqrt(num1)}");
+                    break;
+                case "7":
+                    Console.Write("Введите число для вычисления факториала: ");
+                    intNum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Результат: {Factorial(intNum)}");
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                    break;
+            }
+        }
+    }
+}
---
+++ CalculatorTest/Tests.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+        {
+            // Arrange
+            double a = 5;
+            double b = 3;
+            
+            // Act
+            double result = Calculator.Add(a, b);
+            
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void Add_NegativeAndPositiveNumber_ReturnsCorrectSum()
+        {
+            Assert.AreEqual(2, Calculator.Add(5, -3));
+        }
+
+        [Test]
+        public void Add_TwoNegativeNumbers_ReturnsCorrectSum()
+        {
+            Assert.AreEqual(2, Calculator.Add(-5, -3));
+        }
+
+        [Test]
+        public void Subtract_TwoNumbers_ReturnsCorrectDifference()
+        {
+            Assert.AreEqual(2, Calculator.Subtract(5, 3));
+        }
+
+        [Test]
+        public void Multiply_TwoNumbers_ReturnsCorrectProduct()
+        {
+            Assert.AreEqual(15, Calculator.Multiply(5, 3));
+        }
+
+        [Test]
+        public void Divide_TwoNumbers_ReturnsCorrectQuotient()
+        {
+            Assert.AreEqual(2, Calculator.Divide(6, 3));
+        }
+
+        [Test]
+        public void Divide_TwonNegativeNumbers_ReturnsCorrectQuotient()
+        {
+            Assert.AreEqual(3, Calculator.Divide(-1, -3));
+        }
+        
+        
+
+        [Test]
+        public void Divide_ByZero_ShouldReturnInfinity()
+        {
+            double number = 5;
+            double zero = 0;
+            double result = Calculator.Divide(number, zero);
+            bool isInfinity = double.IsPositiveInfinity(result);
+            Assert.IsTrue(isInfinity, "Деление на ноль должно давать бесконечность");
+        }
+
+        [Test]
+        public void Power_NumberToPower_ReturnsCorrectValue()
+        {
+            Assert.AreEqual(8, Calculator.Power(2, 3));
+        }
+
+        [Test]
+        public void Power_WithTwoNegativeNumbers_ReturnsCorrectPower()
+        {
+            Assert.AreEqual(-1, Calculator.Power(-1, -3));
+        }
+
+        [Test]
+        public void Power_WithNegativeNumber_ReturnsCorrectPower()
+        {
+            Assert.AreEqual(0.008, Calculator.Power(5, -3), 0.0001);
+        }
+
+
+        [Test]
+        public void Sqrt_Number_ReturnsCorrectSquareRoot()
+        {
+            Assert.AreEqual(3, Calculator.Sqrt(9));
+        }
+
+        [Test]
+        public void Sqrt_NegativeNumber_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Calculator.Sqrt(-1));
+        }
+
+
+        [Test]
+        public void Factorial_PositiveNumber_ReturnsCorrectValue()
+        {
+            Assert.AreEqual(120, Calculator.Factorial(5));
+        }
+
+        [Test]
+        public void Factorial_Zero_ReturnsOne()
+        {
+            Assert.AreEqual(1, Calculator.Factorial(0));
+        }
+
+        [Test]
+        public void Factorial_NegativeNumber_ReturnsOne()
+        {
+            Assert.AreEqual(1, Calculator.Factorial(-5));
+        }
+    }
+}
