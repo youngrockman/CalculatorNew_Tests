c39e69a9037211453825eb3586d136f11eee50bb
Klim <karelian_company@mail.ru>
2025-03-27 13:14:27 +0300

    test_finish

--- CalculatorTest/Tests.cs
+++ CalculatorTest/Tests.cs
@@ -1,153 +1,276 @@
-﻿using NUnit.Framework;
-using System;
-using NUnit.Framework.Internal;
-
-namespace CalculatorTests
+﻿public class CalculatorTests
 {
-    [TestFixture]
-    public class CalculatorTests
+    [Test]
+    public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
     {
-        [Test]
-        public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
-        {
-            // Arrange
-            double a = 5;
-            double b = 3;
-            
-            // Act
-            double result = Calculator.Add(a, b);
-            
-            // Assert
-            Assert.AreEqual(8, result);
-        }
+        double a = 5;
+        double b = 3;
+        double result = Calculator.Add(a, b);
+        Assert.AreEqual(8, result);
+    }
 
-        [Test]
-        public void Add_NegativeAndPositiveNumber_ReturnsCorrectSum()
-        {
-            Assert.AreEqual(2, Calculator.Add(5, -3));
-        }
+    [Test]
+    public void Add_TwoNegativeNumbers_ReturnsCorrectSum()
+    {
+        double a = -5;
+        double b = -3;
+        double result = Calculator.Add(a, b);
+        Assert.AreEqual(-8, result);
+    }
 
-        [Test]
-        public void Add_TwoNegativeNumbers_ReturnsCorrectSum()
-        {
-            Assert.AreEqual(-8, Calculator.Add(-5, -3));
-        }
+    [Test]
+    public void Add_PositiveAndNegativeNumber_ReturnsCorrectSum()
+    {
+        double a = 5;
+        double b = -3;
+        double result = Calculator.Add(a, b);
+        Assert.AreEqual(2, result);
+    }
 
-        [Test]
-        public void Subtract_TwoNumbers_ReturnsCorrectDifference()
-        {
-            Assert.AreEqual(2, Calculator.Subtract(5, 3));
-        }
+    [Test]
+    public void Add_ZeroAndNumber_ReturnsSameNumber()
+    {
+        double a = 0;
+        double b = 7;
+        double result = Calculator.Add(a, b);
+        Assert.AreEqual(7, result);
+    }
 
-        [Test]
-        public void Subtract_TwoNegativeNumbers_ReturnsCorrectDifference()
-        {
-            Assert.AreEqual(0, Calculator.Subtract(-2, -2));
-        }
+    [Test]
+    public void Subtract_TwoPositiveNumbers_ReturnsCorrectDifference()
+    {
+        // Arrange
+        double a = 5;
+        double b = 3;
 
-        [Test]
-        public void Subtract_OneNegativeNumber_ReturnsCorrectDifference()
-        {
-            Assert.AreEqual(8, Calculator.Subtract(5, -3));
-        }
+        // Act
+        double result = Calculator.Subtract(a, b);
 
-        [Test]
-        public void Multiply_TwoNumbers_ReturnsCorrectProduct()
-        {
-            Assert.AreEqual(15, Calculator.Multiply(5, 3));
-        }
+        // Assert
+        Assert.AreEqual(2, result);
+    }
 
-        [Test]
-        public void Multiply_TwoNegativeNumbers_ReturnsCorrectProduct()
-        {
-            Assert.AreEqual(4, Calculator.Multiply(-2, -2));
-        }
+    [Test]
+    public void Subtract_TwoNegativeNumbers_ReturnsCorrectDifference()
+    {
+        double a = -5;
+        double b = -3;
+        double result = Calculator.Subtract(a, b);
+        Assert.AreEqual(-2, result);
+    }
 
-        [Test]
-        public void Multiply_WithZeroNumber_ReturnsCorrectProduct()
-        {
-            Assert.AreEqual(0, Calculator.Multiply(5, 0));
-        }
+    [Test]
+    public void Subtract_PositiveAndNegativeNumber_ReturnsCorrectDifference()
+    {
+        double a = 5;
+        double b = -3;
+        double result = Calculator.Subtract(a, b);
+        Assert.AreEqual(8, result);
+    }
 
-        [Test]
-        public void Multiply_WithOneNegativeNumber_ReturnsCorrectProduct()
-        {
-            Assert.AreEqual(-30, Calculator.Multiply(10, -3));
-        }
+    [Test]
+    public void Multiply_TwoPositiveNumbers_ReturnsCorrectProduct()
+    {
+        double a = 5;
+        double b = 3;
+        double result = Calculator.Multiply(a, b);
+        Assert.AreEqual(15, result);
+    }
 
-        [Test]
-        public void Divide_TwoNumbers_ReturnsCorrectQuotient()
-        {
-            Assert.AreEqual(2, Calculator.Divide(6, 3));
-        }
+    [Test]
+    public void Multiply_TwoNegativeNumbers_ReturnsCorrectProduct()
+    {
+        double a = -5;
+        double b = -3;
+        double result = Calculator.Multiply(a, b);
+        Assert.AreEqual(15, result);
+    }
 
+    [Test]
+    public void Multiply_PositiveAndNegativeNumber_ReturnsCorrectProduct()
+    {
+        double a = 5;
+        double b = -3;
+        double result = Calculator.Multiply(a, b);
+        Assert.AreEqual(-15, result);
+    }
 
-        [Test]
-        public void Divide_TwoNegativeNumbers_ReturnsCorrectQuotient()
-        {
-            Assert.AreEqual(3, Calculator.Divide(-1, -3));
-        }
-        
-        
+    [Test]
+    public void Multiply_ByZero_ReturnsZero()
+    {
+        double a = 5;
+        double b = 0;
+        double result = Calculator.Multiply(a, b);
+        Assert.AreEqual(0, result);
+    }
 
-        [Test]
-        public void Divide_ByZero_ShouldReturnInfinity()
-        {
-            double number = 5;
-            double zero = 0;
-            double result = Calculator.Divide(number, zero);
-            bool isInfinity = double.IsPositiveInfinity(result);
-            Assert.IsTrue(isInfinity, "Деление на ноль должно давать бесконечность");
-        }
+    [Test]
+    public void Divide_TwoPositiveNumbers_ReturnsCorrectQuotient()
+    {
+        double a = 6;
+        double b = 3;
+        double result = Calculator.Divide(a, b);
+        Assert.AreEqual(2, result);
+    }
 
-        [Test]
-        public void Power_NumberToPower_ReturnsCorrectValue()
-        {
-            Assert.AreEqual(8, Calculator.Power(2, 3));
-        }
+    [Test]
+    public void Divide_TwoNegativeNumbers_ReturnsCorrectQuotient()
+    {
+        double a = -6;
+        double b = -3;
+        double result = Calculator.Divide(a, b);
+        Assert.AreEqual(2, result);
+    }
 
-        [Test]
-        public void Power_WithTwoNegativeNumbers_ReturnsCorrectPower()
-        {
-            Assert.AreEqual(-1, Calculator.Power(-1, -3));
-        }
+    [Test]
+    public void Divide_PositiveByNegativeNumber_ReturnsCorrectQuotient()
+    {
+        double a = 6;
+        double b = -3;
+        double result = Calculator.Divide(a, b);
+        Assert.AreEqual(-2, result);
+    }
 
-        [Test]
-        public void Power_WithNegativeNumber_ReturnsCorrectPower()
-        {
-            Assert.AreEqual(0.008, Calculator.Power(5, -3), 0.0001);
-        }
+    [Test]
+    public void Divide_ByOne_ReturnsSameNumber()
+    {
+        double a = 5;
+        double b = 1;
+        double result = Calculator.Divide(a, b);
+        Assert.AreEqual(5, result);
+    }
 
+    [Test]
+    public void Divide_ByZero_ThrowsException()
+    {
+        double a = 5;
+        double b = 0;
+        Assert.Throws<DivideByZeroException>(() => Calculator.Divide(a, b));
+    }
 
-        [Test]
-        public void Sqrt_Number_ReturnsCorrectSquareRoot()
-        {
-            Assert.AreEqual(3, Calculator.Sqrt(9));
-        }
+    [Test]
+    public void Divide_ZeroByNumber_ReturnsZero()
+    {
+        double a = 0;
+        double b = 5;
+        double result = Calculator.Divide(a, b);
+        Assert.AreEqual(0, result);
+    }
 
-        [Test]
-        public void Sqrt_NegativeNumber_ThrowsArgumentOutOfRangeException()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Calculator.Sqrt(-1));
-        }
+    [Test]
+    public void Power_PositiveNumberToPositivePower_ReturnsCorrectResult()
+    {
+        double a = 2;
+        double b = 3;
+        double result = Calculator.Power(a, b);
+        Assert.AreEqual(8, result);
+    }
 
+    [Test]
+    public void Power_PositiveNumberToZeroPower_ReturnsOne()
+    {
+        double a = 5;
+        double b = 0;
+        double result = Calculator.Power(a, b);
+        Assert.AreEqual(1, result);
+    }
 
-        [Test]
-        public void Factorial_PositiveNumber_ReturnsCorrectValue()
-        {
-            Assert.AreEqual(120, Calculator.Factorial(5));
-        }
+    [Test]
+    public void Power_ZeroToPositivePower_ReturnsZero()
+    {
+        double a = 0;
+        double b = 5;
+        double result = Calculator.Power(a, b);
+        Assert.AreEqual(0, result);
+    }
 
-        [Test]
-        public void Factorial_Zero_ReturnsOne()
-        {
-            Assert.AreEqual(1, Calculator.Factorial(0));
-        }
+    [Test]
+    public void Power_NegativeNumberToEvenPower_ReturnsPositiveResult()
+    {
+        double a = -2;
+        double b = 4;
+        double result = Calculator.Power(a, b);
+        Assert.AreEqual(16, result);
+    }
 
-        [Test]
-        public void Factorial_NegativeNumber_ReturnsOne()
-        {
-            Assert.AreEqual(1, Calculator.Factorial(-5));
-        }
+    [Test]
+    public void Power_NegativeNumberToOddPower_ReturnsNegativeResult()
+    {
+        double a = -2;
+        double b = 3;
+        double result = Calculator.Power(a, b);
+        Assert.AreEqual(-8, result);
+    }
+
+    [Test]
+    public void Power_ZeroToZeroPower_ThrowsException()
+    {
+        double a = 0;
+        double b = 0;
+        Assert.Throws<ArgumentException>(() => Calculator.Power(a, b));
+    }
+
+    [Test]
+    public void Factorial_PositiveNumber_ReturnsCorrectResult()
+    {
+        int n = 5;
+        double result = Calculator.Factorial(n);
+        Assert.AreEqual(120, result);
+    }
+
+    [Test]
+    public void Factorial_Zero_ReturnsOne()
+    {
+        int n = 0;
+        double result = Calculator.Factorial(n);
+        Assert.AreEqual(1, result);
+    }
+
+    [Test]
+    public void Factorial_One_ReturnsOne()
+    {
+        int n = 1;
+        double result = Calculator.Factorial(n);
+        Assert.AreEqual(1, result);
+    }
+
+    [Test]
+    public void Factorial_NegativeNumber_ThrowsException()
+    {
+        int n = -5;
+        Assert.Throws<ArgumentException>(() => Calculator.Factorial(n));
+    }
+
+    [Test]
+    public void Sqrt_PositiveNumber_ReturnsCorrectResult()
+    {
+        double a = 16;
+        double result = Calculator.Sqrt(a);
+        Assert.AreEqual(4, result);
+    }
+
+    [Test]
+    public void Sqrt_Zero_ReturnsZero()
+    {
+        double a = 0;
+        double result = Calculator.Sqrt(a);
+        Assert.AreEqual(0, result);
+    }
+
+    [Test]
+    public void Sqrt_NegativeNumber_ThrowsException()
+    {
+        double a = -16;
+        Assert.Throws<ArgumentException>(() => Calculator.Sqrt(a));
+    }
+
+    [Test]
+    public void Sqrt_FractionalNumber_ReturnsCorrectResult()
+    {
+        double a = 2.25;
+        double result = Calculator.Sqrt(a);
+        Assert.AreEqual(1.5, result);
     }
 }
+    
+    
